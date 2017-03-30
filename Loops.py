5197e880c9cda7cb4829bf47b07c19542b4336e0
Ben <benjohnson13@outlook.com>
2017-03-30 17:43:55 -0500

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,58 +1,124 @@
-# Input and Output
+# Loops
 
-# Here we have the "print" function with a single parameter; the text value, 'Hello World!'
-# You can learn more at: https://docs.python.org/2/library/functions.html#print
-print('Hello World!')
+# Computers make quick, efficient, and accurate calculation machines that can perform
+# repetitive tasks over and over and over. They're great at performing math calculations
+# where a human might take a long time and be prone to make errors. Once a computer has
+# been taught something, it can perform that calculation very quickly. 
+#
+# The key comes in providing the computer with appropriate instructions. Loops are a good 
+# way to provide instructutions for these kind of tasks and allow us to simplify those 
+# instructions. Rather than having to write out hundreds of line of code with multiple "if"
+# statements, we can create a few lines of logic that utilize a few built in functions.
+#
+# There are many ways we could write out "for loops" and "while loops". But it's always
+# best practice to try to make your code as simple, readable and powerful as agile as 
+# possible. Below are just a few examples of ways we could use loops in our programs.    
 
-# When looking at the "print" function documentation, it has a couple of cool named-parameters
-# that can be added. For example in Python 3.x,
-print('Hello', 'World', '!', sep='')
+
+# Here we have a simple list of names and a "for loop" for them.
+names = ['Alice', 'Bob', 'Cathy', 'Doug']
+
+print (names)
+
+for name in names:
+	print(name, 'is  great person.')
+
+# Output
+# ['Alice', 'Bob', 'Cathy', 'Doug']
+# Alice is  great person.
+# Bob is  great person.
+# Cathy is  great person.
+# Doug is  great person.
+
+
+# We could also do this with numbers using the same list and loop.
+numbers = [1, 2, 3, 4]
+
+print (numbers)
+
+for number in numbers:
+	print(number)
+
+# Output
+# [1, 2, 3, 4]
+# 1
+# 2
+# 3
+# 4
+
+
+# Now I'll simplify this "for loop" with the range function (see docs below to learn more).
+# https://docs.python.org/2/library/functions.html#range
+numbers = range(1, 5)
+
+print (numbers)
+
+for number in numbers:
+	print(number)
 
 # Output:
-# HelloWorld!
+# [1, 2, 3, 4]
+# 1
+# 2
+# 3
+# 4
 
-# I gave the sep='' parameter a non-space string argument, which smooshed the values together 
-# with no spaces. Another named-parameter, 
-print('Hello', 'World', '!', end='')
-print('Hello', 'World', '!', sep='')
+
+# Here is a "for loop" test for numbers to see if the number is a prime number between 2 and 10.
+num = 3
+# Note Python is case sensitive here for True and False
+prime = True 
+
+for test in range(2, 10):
+	if num % test == 0 and num != test:
+		print(num, 'equals', test, '*', num/test)
+		prime = False
+
+if prime: 
+	print(num, 'is a prime number!')
+else:
+	print(num, 'is not a prime number.')
 
 # Output:
-# HelloWorld!HelloWorld!
+# 3 is a prime number!
 
-# Normally there is a line break when a new print statement begins, but in this case
-# I explicitely told the program to have no spaces or new line breaks with the non-
-# space string argument in the end'' named parameter.
 
-# Now I want to call the "compare" function to compare multiple values and will separate 
-# the values with commas to establish parameters.
-cmp(11, 13)
+# Here I want test against a larger number and use the break command to prevent the code from
+# running five thousand times (if it's not a prime number) and simplify the loop even more. 
+# https://docs.python.org/2/reference/simple_stmts.html#break
+num = 5011
 
-# Output:
-# -1
+for test in range(2, num):
+	if num % test == 0 and num != test:
+		print(num, 'equals', test, '*', num/test)
+		print(num, 'is not a prime number.')
+		break
 
-# Some functions can only accept one value, others might take two, or as many as you want.
-# To look at some other functions, or to understand them better, check out the Python 
-# documentation here: https://docs.python.org/2/library/functions.html#cmp
+else: 
+	print(num, 'is a prime number!')
 
-# Now let's create a program that will allow us to get varying input from our users 
-# instead instead of just playing with static code. And since I am using Python 3.6 
-# in this example, I will be using the 'input' function instead of 'raw_input' which 
-# is needed for Python 2.x versions.
-userName = input('Please enter your name: ' )
-age = int(input('Please enter your age: '))
+# Output
+# 5011 is a prime number!
 
-factor = 2 
-finalAge = age + factor
-multAge = age * factor
-divAge = age / factor
 
-print('In', factor, 'years you will be', finalAge, 'years old', userName)
-print('Your age multiplied by', factor, 'is', multAge)
-print('Your age divided by', factor, 'is', divAge)
+# Now let's take a look at a "while loop".
 
-# Output: 
-# Please enter your name: Ben 
-# Please enter your age: 27
-# In 2 years you will be 29 years old Ben
-# Your age multiplied by 2 is 54
-# Your age divided by 2 is 13.5
+sum = 0
+add = 1
+
+while add != 0:
+	print('The current sum is:', sum)
+	print('How much would you like add now?', end = '')
+	raw_add = input(raw_add)
+	sum = sum + add
+
+# Output
+# The current sum is: 0
+# How much would you like add now?(Type 0 to end the program): 1
+# The current sum is: 1
+# How much would you like add now?(Type 0 to end the program): 2
+# The current sum is: 3
+# How much would you like add now?(Type 0 to end the program): 3
+# The current sum is: 6
+# How much would you like add now?(Type 0 to end the program): 0
+# >>> 
