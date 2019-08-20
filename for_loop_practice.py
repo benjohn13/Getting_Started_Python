b19b670a8f0f1092546563e1987c4e805afdefe4
Wizard <benjohn13@protonmail.com>
2019-08-19 22:03:39 -0700

    Add files via upload

---
+++ for_loop_practice.py
@@ -0,0 +1,19 @@
+# for loop practice
+
+# list of words
+a = ["apple", "banana", "carrot", "cranberries", "raisins", "walnuts"]
+
+for i in a:
+	print(i)
+
+
+
+# list of numbers
+
+b = [1, 3, 6, 9, 11, 13]
+
+total = 0
+
+for i in b:
+	total = total + b
+print(total)
